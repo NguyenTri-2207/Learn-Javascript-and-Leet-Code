33f2aa6bde01b86a85d9e5250a4823ed924faeeb
Your <ngoctri2207@gmail.com>
2024-09-03 23:46:45 +0700

    create 10 project mini with JavaScript

---
+++ 10Project/data.js
@@ -0,0 +1,43 @@
+const dataTab = [
+  {
+    name: "Tic-Tac-Toe",
+    id: "ticTacToe",
+  },
+  {
+    name: "Todo List App",
+    id: "2",
+  },
+  {
+    name: "Weather Dashboard",
+    id: "3",
+  },
+  {
+    name: "Responsive Navbar",
+    id: "4",
+  },
+  {
+    name: "Portfolio Website",
+    id: "5",
+  },
+  {
+    name: "Calculator",
+    id: "6",
+  },
+  {
+    name: "Interactive Quiz",
+    id: "7",
+  },
+  {
+    name: "E-commerce Product Page",
+    id: "8",
+  },
+  {
+    name: "Drag and Drop Interface",
+    id: "9",
+  },
+  {
+    name: "Chat Application",
+    id: "10",
+  },
+];
+export { dataTab };
